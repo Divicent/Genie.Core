36e3afd2838326089f9eae411c6691ce2219c674
Shanaka Rusith <srlsooriyagoda@gmail.com>
2018-06-22 16:48:55 +0530

    Added the time filter

--- Genie.Core/Infrastructure/Filters/Abstract/ITimeSpanFilter.cs
+++ Genie.Core/Infrastructure/Filters/Abstract/ITimeSpanFilter.cs
@@ -1,98 +1,96 @@
-using System;
+﻿using System;
 
 namespace Genie.Core.Infrastructure.Filters.Abstract
 {
     /// <summary>
-    /// Helps to apply filters on a Date attribute to a query
+    /// Helps to apply filters on a boolean attribute to a query
     /// </summary>
     /// <typeparam name="T">Type of the filter context</typeparam>
     /// <typeparam name="TQ">Type of the query context</typeparam>
-    public interface IDateFilter<out T, out TQ> where T : IFilterContext
+    public interface ITimeSpanFilter<out T, out TQ> where T : IFilterContext
     {
         /// <summary>
-        /// Adds an expression to the query to accept items where the current attribute's value is equal to the value of <paramref name="date"/>
+        /// Adds an expression to the query to accept items where the current attribute's value is equal to the value of <paramref name="time"/>
         /// </summary>
-        /// <param name="date">Value to apply</param>
+        /// <param name="time">Value to apply</param>
         /// <returns>A join to continue the query</returns>
-        IExpressionJoin<T, TQ> EqualsTo(DateTime date);
-        
-        /// <summary>
-        /// Adds an expression to the query to accept items where the current attribute's value is not equal to the value of <paramref name="date"/>
-        /// </summary>
-        /// <param name="date">Value to apply</param>
-        /// <returns>A join to continue the query</returns>
-        IExpressionJoin<T, TQ> NotEquals(DateTime date);
+        IExpressionJoin<T, TQ> EqualsTo(TimeSpan time);
 
         /// <summary>
-        /// Adds an expression to the query to accept items where the current attribute's value is larger than the value of <paramref name="dateTime"/>
+        /// Adds an expression to the query to accept items where the current attribute's value is not equal to the value of <paramref name="time"/>
         /// </summary>
-        /// <param name="dateTime">Value to apply</param>
+        /// <param name="time">Value to apply</param>
         /// <returns>A join to continue the query</returns>
-        IExpressionJoin<T, TQ> LargerThan(DateTime dateTime);
-        
+        IExpressionJoin<T, TQ> NotEquals(TimeSpan time);
+
         /// <summary>
-        /// Adds an expression to the query to accept items where the current attribute's value is less than the value of <paramref name="date"/>
+        /// Adds an expression to the query to accept items where the current attribute's value is larger than the value of <paramref name="timeSpan"/>
         /// </summary>
-        /// <param name="date">Value to apply</param>
+        /// <param name="timeSpan">Value to apply</param>
         /// <returns>A join to continue the query</returns>
-        IExpressionJoin<T, TQ> LessThan(DateTime date);
-        
+        IExpressionJoin<T, TQ> LargerThan(TimeSpan timeSpan);
+
         /// <summary>
-        /// Adds an expression to the query to accept items where the current attribute's value is larger than or equal to the value of <paramref name="date"/>
+        /// Adds an expression to the query to accept items where the current attribute's value is less than the value of <paramref name="time"/>
         /// </summary>
-        /// <param name="date">Value to apply</param>
+        /// <param name="time">Value to apply</param>
         /// <returns>A join to continue the query</returns>
-        IExpressionJoin<T, TQ> LargerThanOrEqualTo(DateTime date);
-        
+        IExpressionJoin<T, TQ> LessThan(TimeSpan time);
+
         /// <summary>
-        /// Adds an expression to the query to accept items where the current attribute's value is less than or equal to the value of <paramref name="date"/>
+        /// Adds an expression to the query to accept items where the current attribute's value is larger than or equal to the value of <paramref name="time"/>
         /// </summary>
-        /// <param name="date">Value to apply</param>
+        /// <param name="time">Value to apply</param>
         /// <returns>A join to continue the query</returns>
-        IExpressionJoin<T, TQ> LessThanOrEqualTo(DateTime date);
-        
+        IExpressionJoin<T, TQ> LargerThanOrEqualTo(TimeSpan time);
+
+        /// <summary>
+        /// Adds an expression to the query to accept items where the current attribute's value is less than or equal to the value of <paramref name="time"/>
+        /// </summary>
+        /// <param name="time">Value to apply</param>
+        /// <returns>A join to continue the query</returns>
+        IExpressionJoin<T, TQ> LessThanOrEqualTo(TimeSpan time);
+
         /// <summary>
         /// Adds an expression to the query to accept items where the current attribute's value is in between the value of <paramref name="from"/> and <paramref name="to"/>.
         /// </summary>
         /// <param name="from">The from value</param>
         /// <param name="to">The to value</param>
         /// <returns>A join to continue the query</returns>
-        IExpressionJoin<T, TQ> Between(DateTime from, DateTime to);
-        
+        IExpressionJoin<T, TQ> Between(TimeSpan from, TimeSpan to);
+
         /// <summary>
         /// Adds an expression to the query to accept items where the current attribute's value is null (no value)
         /// </summary>
         /// <returns>A join to continue the query</returns>
         IExpressionJoin<T, TQ> IsNull();
-        
+
         /// <summary>
         /// Adds an expression to the query to accept items where the current attribute's value is not null
         /// </summary>
         /// <returns>A join to continue the query</returns>
         IExpressionJoin<T, TQ> IsNotNull();
-		    
-            
+
+
         /// <summary>
         /// Adds an expression to the query to accept items where the current attribute's value is null or not depending on the passed value to the parameter <paramref name="isNull"/>
         /// </summary>
         /// <param name="isNull">true is null while false is not null</param>
         /// <returns>A join to continue the query</returns>
         IExpressionJoin<T, TQ> IsNull(bool isNull);
-        
+
         /// <summary>
-        /// Adds an expression to the query to accept items where the current attribute's value is in the <paramref name="value"/> array
+        /// Adds an expression to the query to accept items where the current attribute's value is in the <paramref name="items"/> array
         /// </summary>
         /// <param name="items">Values to check</param>
         /// <returns>A join to continue the query</returns>
-        IExpressionJoin<T, TQ> In(params DateTime[] items);
-        
+        IExpressionJoin<T, TQ> In(params TimeSpan[] items);
+
         /// <summary>
-        /// Adds an expression to the query to accept items where the current attribute's value is not in the <paramref name="value"/> array
+        /// Adds an expression to the query to accept items where the current attribute's value is not in the <paramref name="items"/> array
         /// </summary>
         /// <param name="items">Values to check</param>
         /// <returns>A join to continue the query</returns>
-        IExpressionJoin<T, TQ> NotIn(params DateTime[] items);
+        IExpressionJoin<T, TQ> NotIn(params TimeSpan[] items);
     }
 }
-
- 
---
+++ Genie.Core/Infrastructure/Filters/Concrete/TimeSpanFilter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+using Genie.Core.Infrastructure.Filters.Abstract;
+
+namespace Genie.Core.Infrastructure.Filters.Concrete
+{
+    public class TimeSpanFilter<T, TQ> : ITimeSpanFilter<T, TQ> where T : IFilterContext
+    {
+        private readonly string _propertyName;
+        private readonly T _parent;
+        private readonly TQ _q;
+
+        public TimeSpanFilter(string propertyName, T parent, TQ q)
+        {
+            _parent = parent;
+            _propertyName = propertyName;
+            _q = q;
+        }
+
+        public IExpressionJoin<T, TQ> EqualsTo(TimeSpan time)
+        {
+            _parent.Add(QueryMaker.EqualsTo(_propertyName, time, true));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> NotEquals(TimeSpan time)
+        {
+            _parent.Add(QueryMaker.NotEquals(_propertyName, time, true));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> LargerThan(TimeSpan time)
+        {
+            _parent.Add(QueryMaker.GreaterThan(_propertyName, time, true));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> LessThan(TimeSpan time)
+        {
+            _parent.Add(QueryMaker.LessThan(_propertyName, time, true));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> LargerThanOrEqualTo(TimeSpan time)
+        {
+            _parent.Add(QueryMaker.GreaterThanOrEquals(_propertyName, time, true));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> LessThanOrEqualTo(TimeSpan time)
+        {
+            _parent.Add(QueryMaker.LessThanOrEquals(_propertyName, time, true));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> Between(TimeSpan from, TimeSpan to)
+        {
+            _parent.Add(QueryMaker.Between(_propertyName, from, to, true));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> IsNull()
+        {
+            _parent.Add(QueryMaker.IsNull(_propertyName));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> IsNotNull()
+        {
+            _parent.Add(QueryMaker.IsNotNull(_propertyName));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> IsNull(bool isNull)
+        {
+            _parent.Add(isNull ? QueryMaker.IsNull(_propertyName) : QueryMaker.IsNotNull(_propertyName));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> In(params TimeSpan[] items)
+        {
+            _parent.Add(QueryMaker.In(_propertyName, items.Cast<object>().ToArray(), true));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+
+        public IExpressionJoin<T, TQ> NotIn(params TimeSpan[] items)
+        {
+            _parent.Add(QueryMaker.NotIn(_propertyName, items.Cast<object>().ToArray(), true));
+            return new ExpressionJoin<T, TQ>(_parent, _q);
+        }
+    }
+}
